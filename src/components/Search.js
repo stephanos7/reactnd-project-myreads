6e6ca16c07edcdf0469943d3da52e0b7303a7b4e
Stephanos Theodotou <stephanos.theodotou@gmail.com>
2018-04-29 18:05:23 +0100

    match search query with relevant books searching in either the title or author fields and render the relevant list onChange of the search term

--- src/components/Search.js
+++ src/components/Search.js
@@ -10,33 +10,26 @@ class Search extends React.Component{
 
   updateQuery = (query) => {
     this.setState(() => ({
-      query: query.trim()
+      query: query
     }))
   }
 
   render(){
     const {books, updateReadingStatus} = this.props;
+    const filterTerm = this.state.query.trim().toLowerCase();
     return (
       <div className="list-books">
         <div className="search-books">
           <div className="search-books-bar">
             <Link className="close-search" to="/">Close</Link>
               <div className="search-books-input-wrapper">
-                  {/*
-                    NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                    You can find these search terms here:
-                    https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                    However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                    you don't find a specific author or title. Every search is limited by search terms.
-                  */}
                 <input type="text" value={this.state.query} onChange={(e) => this.updateQuery(e.target.value)} placeholder="Search by title or author"/>
               </div>
             </div>
           <div className="search-books-results">
             <ol className="books-grid">
             {books.filter( book => (
-              book.title.includes(this.state.query.trim()) ||  book.authors[0].includes(this.state.query.trim())
+              book.title.toLowerCase().includes(filterTerm) ||  book.authors[0].toLowerCase().includes(filterTerm)
             )).map( book => (
               <Book key={book.id}
                     book={book}
