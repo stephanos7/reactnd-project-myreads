d51507632e49f0fb10d8d08abc65cee072153225
Stephanos Theodotou <stephanos.theodotou@gmail.com>
2018-04-29 14:27:34 +0100

    add ListBooks components and refactor promise resolve in updateReadingStatus to trigger updateAllBooks as a cb to an arrow fn

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,7 @@
 import React from 'react'
-import { Link, Route } from "react-router-dom";
 
 import * as BooksAPI from '../utils/BooksAPI';
-
-import Currently from "./Currently";
-import WantTo from "./WantTo";
-import Read from "./Read";
-import Search from "./Search";
+import ListBooks from "./ListBooks";
 
 import '../styles/App.css';
 
@@ -23,7 +18,7 @@ class App extends React.Component {
 
   updateReadingStatus = (event, id) => {
     BooksAPI.update(id, event.target.value)
-    .then(this.updateAllBooks());
+    .then(() => this.updateAllBooks());
   }
 
   componentDidMount(){
@@ -33,25 +28,10 @@ class App extends React.Component {
   render() {
     return (
       <div className="app">          
-          <div className="list-books">
-          <Search />
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <div>
-                <Currently currentlyReading={this.state.books}
-                           updateReadingStatus={this.updateReadingStatus} />
-                <WantTo wantToRead={this.state.books}
-                        updateReadingStatus={this.updateReadingStatus} />
-                <Read alreadyRead={this.state.books}
-                      updateReadingStatus={this.updateReadingStatus} />
-              </div>
-            </div>
-            <div className="open-search">
-              <Link to="/search" component={Search}>Add a book</Link>
-            </div>
-          </div>
+        <ListBooks books={this.state.books}
+                   updateAllBooks={this.updateAllBooks}
+                   updateReadingStatus={this.updateReadingStatus}
+        />
       </div>
     )
   }
---
+++ src/components/ListBooks.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link, Route } from "react-router-dom";
+
+import Currently from "./Currently";
+import WantTo from "./WantTo";
+import Read from "./Read";
+import Search from "./Search";
+
+const ListBooks = (props) => {
+  return(
+    <div className="list-books">
+      <Search />
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <div>
+            <Currently currentlyReading={props.books}
+                        updateReadingStatus={props.updateReadingStatus} />
+            <WantTo wantToRead={props.books}
+                    updateReadingStatus={props.updateReadingStatus} />
+            <Read alreadyRead={props.books}
+                  updateReadingStatus={props.updateReadingStatus} />
+          </div>
+        </div>
+        {/*<div className="open-search">
+          <Link to="/search" component={Search}>Add a book</Link>
+        </div>*/}
+      </div>
+  )
+}
+
+export default ListBooks;
