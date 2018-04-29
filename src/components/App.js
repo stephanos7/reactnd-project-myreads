935e23ac7fcf7a41196acebcb735893d046a1c65
Stephanos Theodotou <stephanos.theodotou@gmail.com>
2018-04-29 14:05:59 +0100

    add Search component

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,16 @@
 import React from 'react'
 import * as BooksAPI from '../utils/BooksAPI';
+
 import Currently from "./Currently";
 import WantTo from "./WantTo";
 import Read from "./Read";
+import Search from "./Search";
 
 import '../styles/App.css';
 
 class App extends React.Component {
   state = {
     books: [],
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
   }
 
   updateAllBooks = () => {
@@ -35,30 +30,9 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="app">
-        {/*{this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-              <div className="search-books-input-wrapper">*/}
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                {/*<input type="text" placeholder="Search by title or author"/>
-
-              </div>*/}
-            {/*</div>*/}
-            {/*<div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>*/}
-        {/*) : (*/}
+      <div className="app">          
           <div className="list-books">
+          <Search />
             <div className="list-books-title">
               <h1>MyReads</h1>
             </div>
@@ -76,7 +50,6 @@ class App extends React.Component {
               <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
             </div>*/}
           </div>
-        {/*)}*/}
       </div>
     )
   }
