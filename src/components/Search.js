f89dd8739c02f024211217851b638cc75ff09276
Stephanos Theodotou <stephanos.theodotou@gmail.com>
2018-05-05 12:52:24 +0100

    refactor book mergin between search results and books in my collection

--- src/components/Search.js
+++ src/components/Search.js
@@ -12,6 +12,7 @@ class Search extends React.Component{
 
   state = {
     searchedBooks: [],
+    mergedBooks: [],
     query : ""
   }
 
@@ -23,23 +24,40 @@ class Search extends React.Component{
 
   searchBooks = (query) => {
     BooksAPI.search(query)
-    .then((searchedBooks) => this.updateStateWithSearchedBooks(searchedBooks))
+    .then((searchedBooks) => this.updateStateWithSearchedBooks(searchedBooks, this.props.myBooks))
   }
 
-  updateStateWithSearchedBooks = (searchedBooks=[]) => {
-    this.setState(() => (
-      {searchedBooks}
-    ))
+  updateStateWithSearchedBooks = (searchedBooks=[], myBooks=[]) => {
+    if(Array.isArray(searchedBooks)){
+      // get an array of ids of books in my collection
+      let myBooksIds = myBooks.map( book => book.id);
+
+      //get an array of ids of books in the search results
+      let searchedBooksIds = searchedBooks.map(book => book.id);
+
+      // find which books from the search results are already in my collection
+      let matchesFound = myBooksIds.filter( book => searchedBooksIds.includes(book));
+
+      // filter out these books from the search results
+      let filteredsearchedbooks = searchedBooks.filter( book => !matchesFound.includes(book.id));
+
+      // get the books objects from my collection based on the matched ids
+      let myBookMatches = myBooks.filter( book => matchesFound.includes(book.id));
+
+      // add matches from my collection to searched books
+      let mergedBooks = filteredsearchedbooks.concat(myBookMatches);
+      
+      return this.setState(() => (
+        {mergedBooks: mergedBooks}
+      ))
+    }
   }
 
-  compareBooks = (myBooks=[], searchedBook) => myBooks.indexOf(searchedBook);
 
   render(){
     const { updateReadingStatus } = this.props;
     const { searchedBooks, query } = this.state;
     const { myBooks }  = this.props;
-
-    const foundInMyBooks= () => this.compareBooks(myBooks, searchedBooks)
 
     return (
       <div className="list-books">
@@ -52,13 +70,24 @@ class Search extends React.Component{
             </div>
           <div className="search-books-results">
             <ol className="books-grid">
-            {Array.isArray(searchedBooks) ? searchedBooks.map( book => (
+            {Array.isArray(searchedBooks) ? this.state.mergedBooks.map( book => (
               <Book key={book.id}
                     book={book}
-                    foundInMyBooks={foundInMyBooks()}
                     updateReadingStatus={updateReadingStatus} />
             )) : <h3>No results found</h3>}
             </ol>
+            
+            <div className="search-books-results">
+            <h4>from my books</h4>
+              <ol className="books-grid">
+                {myBooks.map( book => 
+                  <Book key={book.id}
+                    book={book}
+                    updateReadingStatus={updateReadingStatus} />
+                )}
+              </ol>
+            </div>
+
           </div>
         </div>
       </div>
